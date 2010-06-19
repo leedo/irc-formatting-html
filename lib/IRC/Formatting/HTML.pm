b78f218a00d1cad02c2ae58e1ea22f25d0ffc863
leedo <lee@laylward.com>
2010-06-19 15:12:26 -0500

    Bump version number to 0.22

--- lib/IRC/Formatting/HTML.pm
+++ lib/IRC/Formatting/HTML.pm
@@ -14,12 +14,12 @@ IRC::Formatting::HTML - Convert between HTML and IRC formatting
 
 =head1 VERSION
 
-Version 0.21
+Version 0.22
 
 =cut
 
 our @EXPORT_OK = qw/irc_to_html html_to_irc/;
-our $VERSION = '0.21';
+our $VERSION = '0.22';
 
 =head1 SYNOPSIS
 
